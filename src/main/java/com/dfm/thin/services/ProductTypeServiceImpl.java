a7acc3548c403c5fe49cd32b1532075d47ad5176
chen <chengri1105@gmail.com>
2019-04-27 00:46:40 +0800

    crud is integrated.

--- src/main/java/com/dfm/thin/services/ProductTypeServiceImpl.java
+++ src/main/java/com/dfm/thin/services/ProductTypeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dfm.thin.services;
 
 
+import com.dfm.thin.exceptions.ResourceNotFoundException;
 import com.dfm.thin.model.ProductType;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
@@ -15,6 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -33,15 +35,14 @@ public class ProductTypeServiceImpl implements ProductTypeService{
         try (IgniteClient client = Ignition.startClient(cfg)) {
             ClientCache<Long, ProductType> cache = client.getOrCreateCache(CACHE_NAME);
 
-            String sql = "select * from product_type";
+            String sql = "select id, name, description from product_type";
             List<List<?>> res = cache.query(new SqlFieldsQuery(sql).setDistributedJoins(true)).getAll();
 
             for (List<?> next : res) {
                 ProductType productType = new ProductType();
-                productType.setCreatedAt((Timestamp) next.get(0));
-                productType.setUpdatedAt((Timestamp) next.get(1));
-                productType.setName((String) next.get(2));
-                productType.setDescription((String) next.get(3));
+                productType.setId((Long) next.get(0));
+                productType.setName((String) next.get(1));
+                productType.setDescription((String) next.get(2));
                 productTypes.add(productType);
             }
             return productTypes;
@@ -61,7 +62,9 @@ public class ProductTypeServiceImpl implements ProductTypeService{
         ClientConfiguration cfg = new ClientConfiguration().setAddresses("127.0.0.1:10800");
         try (IgniteClient client = Ignition.startClient(cfg)) {
             ClientCache<Long, ProductType> cache = client.getOrCreateCache(CACHE_NAME);
-            return cache.get(id);
+            ProductType p = cache.get(id);
+            p.setId(id);
+            return p;
         }
         catch (ClientException e) {
             System.err.println(e.getMessage());
@@ -77,9 +80,13 @@ public class ProductTypeServiceImpl implements ProductTypeService{
         ClientConfiguration cfg = new ClientConfiguration().setAddresses("127.0.0.1:10800");
         try (IgniteClient client = Ignition.startClient(cfg)) {
             ClientCache<Long, ProductType> cache = client.getOrCreateCache(CACHE_NAME);
-            Long key =  generateId();
+            String sql = "select max(id) + 1 from product_type";
+            Long key = (Long) cache.query(new SqlFieldsQuery(sql).setDistributedJoins(true)).getAll().iterator().next().iterator().next();
+            productType.setId(key);
             cache.put(key, productType);
-            return cache.get(key);
+            ProductType p = cache.get(key);
+            p.setId(key);
+            return p;
         }
         catch (ClientException e) {
             System.err.println(e.getMessage());
@@ -95,8 +102,11 @@ public class ProductTypeServiceImpl implements ProductTypeService{
         ClientConfiguration cfg = new ClientConfiguration().setAddresses("127.0.0.1:10800");
         try (IgniteClient client = Ignition.startClient(cfg)) {
             ClientCache<Long, ProductType> cache = client.getOrCreateCache(CACHE_NAME);
+            body.setId(id);
             cache.replace(id, body);
-            return cache.get(id);
+            ProductType p = cache.get(id);
+            p.setId(id);
+            return p;
         }
         catch (ClientException e) {
             System.err.println(e.getMessage());
@@ -113,8 +123,13 @@ public class ProductTypeServiceImpl implements ProductTypeService{
         try (IgniteClient client = Ignition.startClient(cfg)) {
             ClientCache<Long, ProductType> cache = client.getOrCreateCache(CACHE_NAME);
 
-            cache.remove(id);
-            return ResponseEntity.ok().build();
+            if (cache.containsKey(id)) {
+                cache.remove(id);
+                return ResponseEntity.ok().build();
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+
         }
         catch (ClientException e) {
             System.err.println(e.getMessage());
@@ -127,5 +142,10 @@ public class ProductTypeServiceImpl implements ProductTypeService{
 
     private Long generateId() {
         return ID_GEN.incrementAndGet();
+    }
+
+    private Timestamp now() {
+        Date date = new Date();
+        return new Timestamp(date.getTime());
     }
 }
--- src/test/java/com/dfm/thin/ThinApplicationTests.java
+++ src/test/java/com/dfm/thin/ThinApplicationTests.java
@@ -1,16 +1,46 @@
 package com.dfm.thin;
 
+import com.dfm.thin.model.ProductType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.Assert;
+
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ThinApplicationTests {
 
+    TestRestTemplate template = new TestRestTemplate();
+    Random r = new Random();
+    int[] clusterPorts = new int[] {8090};
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testApi() throws InterruptedException {
+        for (int i = 0; i < 10; i++) {
+            ProductType p = template.postForObject("http://localhost:8090/api/types", createProductType(), ProductType.class);
+            Assert.notNull(p, "Create productType failed");
+            Thread.sleep(100);
+//            ProductType res = template.getForObject("http://localhost:8090/api/types/{id}", ProductType.class, p.getId());
+//            Assert.notNull(res, "Product Type not found");
+        }
+    }
+
+    private ProductType createProductType() {
+        ProductType p = new ProductType();
+        Random r = new Random();
+        int n =  r.nextInt(100000);
+        p.setName("Test name" + n);
+        p.setId(1L);
+        p.setDescription("Test Description" + n);
+        return p;
     }
 
 }
